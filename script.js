b6a6257f02d976855a7381219fae175bda6a719a
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-02 23:41:09 +0100

    Struttura pagina per l'esercizio

--- script.js
+++ script.js
@@ -8,9 +8,34 @@ new Vue({
         doList: [
             "Comprare i biscotti",
             "Comprare il gelato",
-            "Andare in palestra",
         ],
 
+    },
+    methods: {
+        onAddClick() {
+            //Controllo che non sia una stringa vuota 
+            if (this.newDoText.trim() === "") {
+                return alert("Prego inserire del testo");
+            }
+            //Controllo se il mio elemento c'è già
+            const foundElement = this.doList.find((element) => {
+                return element.toLowerCase() === this.newDoText.trim().toLowerCase()
+
+            });
+
+            if (foundElement) {
+                return
+            }
+            //Stampo il mio nuovo elemento
+            this.doList.push(this.newDoText.trim());
+            //Svuoto il box dove scrivere l'elemento
+            this.newDoText = "";
+        },
+
+        //Cancello un'elemento premendo sulla x
+        deleteClick(elementoDaRimuovere) {
+            this.doList.splice(elementoDaRimuovere, 1)
+        }
     },
 
 });
