137af0899a55339f5602003f90794ef25635b105
Ele-nonsochi <elea.luzi25@gmail.com>
2021-11-02 16:43:22 +0100

    Inizio lavoro

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ Vue.config.devtools = true
 new Vue({
     el: "#root",
     data: {
-        welcome: "Hello Vueoolean - Todolist"
-    }
-})
+        welcome: "Hello Vueoolean - Todolist",
+        newDoText: "",
+        doList: [
+            "Comprare i biscotti",
+            "Comprare il gelato",
+            "Andare in palestra",
+        ],
+
+    },
+
+});
